7777a66236b2fb19971e82bf6f7ae0df4d9e3695
colmpat <cplang@dons.usfca.edu>
2021-07-05 11:35:46 -0700

    07/05/21

--- src/AgePredictor.java
+++ src/AgePredictor.java
@@ -1,3 +1,6 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Properties;
 
 public class AgePredictor {
@@ -11,8 +14,32 @@ public class AgePredictor {
             System.out.println("Error, usage: java AgePredictor inputFile");
             return;
         }
-        String path = args[0];
+//        String path = args[0];
+//        File file = new File(path);
+//
+//        Properties properties = new Properties();
+//        String container = properties.getProperty("");
+//
+//
+        //Create new initial properties
+        Properties properties = new Properties();
+        //Open reader to read given file
+        try (FileReader in = new FileReader(args[0])) {
+            //load properties from file
+            properties.load(in);
+        } catch (IOException e) {
+            System.out.println("There was an error with the provided file. Ensure the file is valid.");
+            System.out.println("Error: " + e.getLocalizedMessage());
+        }
 
+        // print out what you just read
+        System.out.println("Properties:");
+        Enumeration<?> propertyNames = properties.propertyNames();
+        while(propertyNames.hasMoreElements()) {
+            String name = propertyNames.nextElement().toString();
+            System.out.println(name + ": " + properties.getProperty(name));
+        }
+        System.out.println("---------------------------------------");
 
     }
 }
