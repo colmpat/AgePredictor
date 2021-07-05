cfe137a1ccc8749b28b96bbf90240fab0ad699bc
colmpat <cplang@dons.usfca.edu>
2021-07-04 19:44:41 -0700

    Initial commit

---
+++ src/AgePredictor.java
@@ -0,0 +1,18 @@
+import java.util.Properties;
+
+public class AgePredictor {
+
+    public AgePredictor() {
+
+    }
+
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.out.println("Error, usage: java AgePredictor inputFile");
+            return;
+        }
+        String path = args[0];
+
+
+    }
+}
