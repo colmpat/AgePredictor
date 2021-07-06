b0f3c7fe1fc4cf434c8b0ef98d9e5940d99ad37c
Colm <colmplang@gmail.com>
2021-07-05 18:51:14 -0700

    07/05/2021 Fully Funcional Implementation

---
+++ src/Name.java
@@ -0,0 +1,15 @@
+public class Name {
+    String state;
+    char sex;
+    int birthYear;
+    String name;
+    int count;
+    
+    public Name(String state, char sex, int birthYear, String name, int count) {
+        this.state = state;
+        this.sex = sex;
+        this.birthYear = birthYear;
+        this.name = name;
+        this.count = count;
+    }
+}
